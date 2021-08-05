41c0f427ee84e46f1a7c7874b63415ed54df1fcc
Joseph Edmonds <consultingjde@gmail.com>
2021-08-05 01:56:51 -0700

    Grid system and viewport breakpoints

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Container } from '@material-ui/core';
 
 function Notes() {
   const [notes, setNotes] = useState([])
@@ -11,11 +14,29 @@ function Notes() {
 
 
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      {/* <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+      <Grid container>
+        {notes.map(note => (
+          <Grid key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
 
