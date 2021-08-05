e6a77fbc78642cf549b568fee28da0f1f8c4331d
Joseph Edmonds <consultingjde@gmail.com>
2021-08-05 00:12:43 -0700

    Buttons, ButtonGroups, and Onclick events

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,8 +1,11 @@
 import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container'
+
 
 function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         color="textSecondary"
@@ -11,7 +14,25 @@ function Create() {
       >
         Create a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log('You clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit" color="primary" >Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined" >Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>two</Button>
+        <Button>three</Button>
+      </ButtonGroup> */}
+    </Container>
   )
 }
 
