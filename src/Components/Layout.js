145e3687dd1aa8a6905347dc517d1f09d643d197
Joseph Edmonds <consultingjde@gmail.com>
2021-08-05 02:36:39 -0700

    Adding a permanent drawer on the left side

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,23 @@
 import { makeStyles } from "@material-ui/core"
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import { flexbox } from "@material-ui/system";
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%'
+  },
+  drawer: {
+    width: drawerWidth
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  root: {
+    display: 'flex',
   }
 })
 
@@ -11,8 +25,23 @@ function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
-      <div className={Classes.page}>
+    <div className={classes.root}>
+      {/* App Bar */}
+
+      {/* Side Drawer */}
+      <Drawer className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant='h5'>
+            Joseph's Notes
+          </Typography>
+        </div>
+      </Drawer>
+
+      <div className={classes.page}>
         {children}
       </div>
     </div>
