57cbf779bc8eed282bb40e5655cc9661ed4723d0
Joseph Edmonds <consultingjde@gmail.com>
2021-08-05 00:41:48 -0700

    createTheme hook, customizing the theme, and importing a google font

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -5,17 +5,7 @@ import { KeyboardArrowRight } from "@material-ui/icons";
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'Violet',
-    '&:hover': {
-      backgroundColor: 'blue'
-    }
-  },
-  title: {
-    textDecoration: "underline",
-    marginBottom: 30
-  }
+
 })
 
 function Create() {
@@ -24,7 +14,6 @@ function Create() {
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -34,7 +23,6 @@ function Create() {
       </Typography>
 
       <Button
-        className={classes.btn}
         onClick={() => console.log('You clicked me')}
         type="submit"
         color="secondary"
